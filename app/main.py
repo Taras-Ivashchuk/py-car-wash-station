b807493b582612a1d9412c923b33668f88ded09a
Taras Ivashchuk <tvishy@disroot.org>
2025-10-26 22:32:45 +0200

    1. Removed unnecessary checks. 2. Removed redundant 'else' statement.

--- app/main.py
+++ app/main.py
@@ -24,26 +24,23 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def wash_single_car(self, car: Car) -> None:
-        if car.clean_mark < self.clean_power:
-            car.clean_mark = self.clean_power
+        car.clean_mark = self.clean_power
 
     def serve_cars(self, cars: list[Car]) -> float:
         total_income = 0.0
         for car in cars:
-            total_income += self.calculate_washing_price(car)
-            self.wash_single_car(car)
+            if car.clean_mark < self.clean_power:
+                total_income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
 
         return round(total_income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        if car.clean_mark < self.clean_power:
-            return round(
-                car.comfort_class * (self.clean_power - car.clean_mark)
-                * (self.average_rating / self.distance_from_city_center)
-                , 1
-            )
-        else:
-            return 0.0
+        return round(
+            car.comfort_class * (self.clean_power - car.clean_mark)
+            * (self.average_rating / self.distance_from_city_center)
+            , 1
+        )
 
     def rate_service(self, new_rate: int) -> None:
         new_total_rate = self.average_rating * self.count_of_ratings + new_rate
