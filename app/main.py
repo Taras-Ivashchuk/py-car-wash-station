a911b6f8ada3416a68583c56aa8633e915ba1ff0
Taras Ivashchuk <tvishy@disroot.org>
2025-10-26 22:01:36 +0200

    1. Refactored wash_single_car 2. Refactored serve_cars

--- app/main.py
+++ app/main.py
@@ -23,20 +23,17 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def wash_single_car(self, car: Car) -> float:
+    def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
-            income = self.calculate_washing_price(car)
             car.clean_mark = self.clean_power
-            return income
-        else:
-            return 0.0
 
     def serve_cars(self, cars: list[Car]) -> float:
         total_income = 0.0
         for car in cars:
-            total_income += self.wash_single_car(car)
+            total_income += self.calculate_washing_price(car)
+            self.wash_single_car(car)
 
-        return total_income
+        return round(total_income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
         if car.clean_mark < self.clean_power:
