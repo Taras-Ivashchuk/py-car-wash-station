4a8eb4c0e23d84dbad38deebef8560aea1f7fdd6
Taras Ivashchuk <tvishy@disroot.org>
2025-10-26 14:51:15 +0200

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,8 +1,55 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int
+    ) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def wash_single_car(self, car: Car) -> float:
+        if car.clean_mark < self.clean_power:
+            income = self.calculate_washing_price(car)
+            car.clean_mark = self.clean_power
+            return income
+        else:
+            return 0.0
+
+    def serve_cars(self, cars: list[Car]) -> float:
+        total_income = 0.0
+        for car in cars:
+            total_income += self.wash_single_car(car)
+
+        return total_income
+
+    def calculate_washing_price(self, car: Car) -> float:
+        if car.clean_mark < self.clean_power:
+            return round(
+                car.comfort_class * (self.clean_power - car.clean_mark)
+                * (self.average_rating / self.distance_from_city_center)
+                , 1
+            )
+        else:
+            return 0.0
+
+    def rate_service(self, new_rate: int) -> None:
+        new_total_rate = self.average_rating * self.count_of_ratings + new_rate
+        new_count_of_ratings = self.count_of_ratings + 1
+        self.average_rating = round(new_total_rate / new_count_of_ratings, 1)
+        self.count_of_ratings = new_count_of_ratings
